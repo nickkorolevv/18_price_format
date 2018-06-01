f620b4446a3f0045258d62fd48e9894344e8c2d5
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-06-01 20:32:57 +0300

    create new script

--- format_price.py
+++ format_price.py
@@ -1,5 +1,33 @@
-def format_price(price):
-    pass
+import re
+import argparse
 
-if __name__ == '__main__':
-    pass
+pattern_letter = re.compile("([a-zA-Zа-яА-ЯёЁ']+)")
+
+
+def format_price(price):
+    if not (isinstance(price, (int, float, str))):
+        raise TypeError("Данный тип не поддерживается")
+    if isinstance(price, str) and pattern_letter.match(price):
+        raise ValueError("Буквы не должны быть введены")
+    if isinstance(price, str)and any(
+        char in "!#$%&'()*+-/:;<=>?@[\]^_`{|}~" for char in price
+    ):
+        raise ValueError("Данные символы не поддеживаются")
+    price = float(price)
+    price = round(price, 2)
+    if price.is_integer():
+        return format(price, ",.0f").replace(",", " ")
+    else:
+        return format(price, ",.2f").replace(",", " ")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--price")
+    parsargs = parser.parse_args()
+    if parsargs.price:
+        price = parsargs.price
+    else:
+        price = input("Введите число:\n")
+    formated_price = format_price(price)
+    print(formated_price)
--- tests.py
+++ tests.py
@@ -1,1 +1,27 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+class PriceFormatTest(unittest.TestCase):
+    def test_int_value(self):
+        self.assertEqual(format_price(123456678), "123 456 678")
+
+    def test_float_value(self):
+        self.assertEqual(format_price(1123.45345), "1 123.45")
+
+    def test_int_string(self):
+        self.assertEqual(format_price("19231985439"), "19 231 985 439")
+
+    def test_float_string(self):
+        self.assertEqual(format_price("12345.6712"), "12 345.67")
+
+    def test_incorrect_str(self):
+        with self.assertRaises(ValueError):
+            format_price("price")
+
+    def test_incorrect_type(self):
+        with self.assertRaises(TypeError):
+            format_price(["123456789"])
+
+
+if __name__ == "__main__":
+    unittest.main()
